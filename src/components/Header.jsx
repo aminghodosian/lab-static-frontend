8e37a021bc4a0069ebb793c9042f43bacf912b48
aminghodosian <63652270+aminghodosian@users.noreply.github.com>
2026-05-14 14:06:47 +0300

    feat: implement Header component and update main layout

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <header className="app-header">
+      <h1>Software Engineering Lab</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
